17047a1349e4a347cbd619d1ddc6ddb9d7e8e82d
VijayaThakur <99168500+VijayaThakur@users.noreply.github.com>
2022-04-12 13:14:03 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+﻿//1. Create an array of all the colors
+var colors 
+
+var currentColor
+
+function setup() {
+  createCanvas(innerWidth, innerHeight);
+  background("white");
+}
+
+
+function draw() {
+  var y =0; 
+  for (i = 0; i < colors.length; i++){
+    //make an object for each color box:
+     currentColor= new band(0,y,400,60,colors[i])
+
+     y = y + 50;
+ }
+}
